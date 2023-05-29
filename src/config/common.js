0d366da531ad51ff27c6f78e8ed86039cc54546d
Undrakhbayar <undrakh0511@gmail.com>
2023-05-29 20:15:28 +0800

    a

--- src/config/common.js
+++ src/config/common.js
@@ -1,6 +1,7 @@
 /* STAGING */
-const REFERENCE_URL = "http://localhost:3500/references";
+//const REFERENCE_URL = "http://localhost:3500/references";
 /* PROD */
+const REFERENCE_URL = "https://mern-api-lcmj.onrender.com/references";
 //
 
 export { REFERENCE_URL };
